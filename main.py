065033f8c0b5e9c4c31c33303413609edc9ac507
jungjunior <50376077+jungjunior@users.noreply.github.com>
2021-09-26 21:13:21 -0300

    Adicionando vosk

---
+++ main.py
@@ -0,0 +1,13 @@
+# arquivo inicial
+
+import speech_recognition as sr
+
+
+# cria um reconhecedor
+r = sr.Recognizer()
+
+# Abrir o microfone para captura
+with sr.Microphone() as source:
+    while True:
+        audio =  r.listen(source) #define microfone como fonte de audio
+        print(r.recognize_google(audio, language='pt'))
